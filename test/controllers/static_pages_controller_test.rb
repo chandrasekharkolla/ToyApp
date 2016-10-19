c47d64b2e9989e40da53766a005c5216d0cf52a5
chandrasekharkolla <chandrasekharkolla@gmail.com>
2016-10-19 01:52:09 -0700

    Changes files according to the rubocop suggetions

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,12 +1,12 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
+  test 'should get home' do
     get static_pages_home_url
     assert_response :success
   end
 
-  test "should get contact" do
+  test 'should get contact' do
     get static_pages_contact_url
     assert_response :success
   end
